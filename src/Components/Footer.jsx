b1cb1885e2ef40424951a2e4ef576edc91bd53bf
naveenpnk <naveenpallagani777@gmail.com>
2024-08-06 22:32:17 +0530

    updated social media links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
         <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] my-12 py-6">
             <p> &copy; 2024 naveen pallagani. All rights reserved.</p>
             <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
-                <li> <a href=""> Git Hub </a> </li>
-                <li> <a href=""> LinkedIn </a> </li>
-                <li> <a href=""> Instagram </a> </li>
+                <li> <a href="https://github.com/naveenpallagani777/" target="blank"> Git Hub </a> </li>
+                <li> <a href="https://www.linkedin.com/in/naveen-pallagani-b23845259/" target="blank"> LinkedIn </a> </li>
+                <li> <a href="https://www.instagram.com/naveenpallagani/" target="blank"> Instagram </a> </li>
             </ul>
         </div>
     </div>)
